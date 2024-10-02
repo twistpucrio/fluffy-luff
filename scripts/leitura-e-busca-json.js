3350d0ef693d3695561c0f64490dde63333d9ff5
Danielle Guimarães Cruz de Oliveira <danielleguimaraes@tecgraf.puc-rio.br>
2024-10-02 18:03:56 -0300

    Adicionando a função de ler do json os elementos e mostras os nomes de cada elemento.

--- scripts/leitura-e-busca-json.js
+++ scripts/leitura-e-busca-json.js
@@ -2,3 +2,28 @@
 
 /*Busca na barra de pesquisa: por nome, por material e por categoria.*/
 /*Todos os Filtros da Página de Resultado.*/
+
+function criaArrayProdutos() {
+    return fetch("../jsons/principal.json")
+        .then(response => response.json())
+        .then(Produtos => {
+            return Produtos.produtos; 
+        })
+        
+}
+
+function buscaPorNome(array) {
+    let dive = document.getElementById("au");
+    let inputTexto = document.getElementById("texto");
+
+    for (let produto of array) {
+        dive.innerHTML += `<p>${produto.nome}</p>`;
+    }
+}
+
+window.addEventListener("load", function() {
+    criaArrayProdutos().then(array => {
+
+        buscaPorNome(array);
+    });
+});
